167868a19c27b6596e85c1a6f1cb5fcac4ef97ad
Samuel Laisaar <slaisaar00@gmail.com>
2021-10-28 14:58:16 +0300

    Tehtävä 2 (abstract factory) valmis

--- SuunnittelumallitKurssi/src/abstractfactory/BossVaateFactory.java
+++ SuunnittelumallitKurssi/src/abstractfactory/BossVaateFactory.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package abstractfactory;
 
-/**
- *
- * @author Samuel
- */
 public class BossVaateFactory implements VaateFactory {
 
     @Override
