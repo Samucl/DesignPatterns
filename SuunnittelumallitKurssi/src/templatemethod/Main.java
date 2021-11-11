573346740938447176b34b588016c3196ef031d5
Samuel Laisaar <slaisaar00@gmail.com>
2021-11-11 14:29:57 +0200

    Tehtävä 8 (templatemethod) valmis.

---
+++ SuunnittelumallitKurssi/src/templatemethod/Main.java
@@ -0,0 +1,8 @@
+package templatemethod;
+
+public class Main {
+    public static void main(String[] args){
+        CoinFlip coinflip = new CoinFlip();
+        coinflip.playOneGame(5); //5 pelaajaa peliin
+    }
+}
